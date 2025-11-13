a6a897bb791829248b804d1cd66cb36efd4e0809
Emmanuel <blaemedia26@gmail.com>
2025-11-13 15:09:14 +0100

    export default users

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import UserCard from "../../components/common/UserCard";
-import  {UserProps}  from "components/common";
+import { UserProps } from '../../interfaces'; 
 
 interface UsersPageProps {
   posts: UserProps[];
 }
 
-export default function UsersPage({ posts }: UsersPageProps) {
+export default users({ posts }: UsersPageProps) {
   return (
     <div className="p-5 grid gap-6 md:grid-cols-2 lg:grid-cols-3">
       {posts.map((user) => (
