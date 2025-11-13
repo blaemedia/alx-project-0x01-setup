9c76afdf47e8de265d07c12ba8aec0d373d84de3
Emmanuel <blaemedia26@gmail.com>
2025-11-13 15:11:38 +0100

    export default users

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -6,7 +6,7 @@ interface UsersPageProps {
   posts: UserProps[];
 }
 
-export default users({ posts }: UsersPageProps) {
+export default Users({ posts }: UsersPageProps) {
   return (
     <div className="p-5 grid gap-6 md:grid-cols-2 lg:grid-cols-3">
       {posts.map((user) => (
